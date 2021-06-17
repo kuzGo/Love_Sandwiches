5871c7fceeaa43af05fc6faad9b658bd95822030
kuzGo <gorankuzma467@gmail.com>
2021-06-17 05:49:24 +0000

    Retrive latest stock number frome stock worksheet

--- run.py
+++ run.py
@@ -1,6 +1,6 @@
 import gspread 
 from google.oauth2.service_account import Credentials
-
+from pprint import pprint
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
     "https://www.googleapis.com/auth/drive.file",
@@ -39,7 +39,7 @@ def validate_data(values):
     
     try:
         [int(value) for value in values]
-        if len(values) !=6:
+        if len(values) != 6:
             raise ValueError(
                 f"Exactly six values required, you provided {len(values)}"
             ) 
@@ -59,7 +59,28 @@ def update_sales_worksheet(data):
     sales_worksheet = SHEET.worksheet('sales')
     sales_worksheet.append_row(data)
     print('Sales worksheet updated sucsessfuly.\n')
-      
-data = get_sales_data()    
-sales_data = [int(num) for num in data]
-update_sales_worksheet(sales_data)
+    
+    
+def calculate_surplus_data(sales_data):
+    """
+    Compare sales with stock and calculate the surplus for each item type.
+    The surplus is defined as the sales figures sutracted from the stock.
+    -Positive surplus indicates waste.
+    - Negative surplus indicates extra made when stock is sold out.
+    """
+    print("Calculating surplus data.....\n")    
+    stock = SHEET.worksheet('stock').get_all_values()
+    stock_row = stock[-1]
+    print(stock_row)
+    
+def main(): 
+    """
+     Run all program functions.
+    """      
+    
+    data = get_sales_data()    
+    sales_data = [int(num) for num in data]
+    update_sales_worksheet(sales_data)
+    calculate_surplus_data(sales_data)
+print("Welcome to Love Sandwiches Data Automation")
+main()    
